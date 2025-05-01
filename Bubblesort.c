fd1f9e07b7c5891b644b63250f9ad82641f51c72
ravikushwaha1 <ravikushwaha13308@gmail.com>
2025-05-01 23:46:16 +0530

    bubble sort

---
+++ Bubblesort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void printArray(int *A,int n){
+for (int i =0;i<n;i++){
+    printf("%d ",A[i]);
+}
+    printf("\n");
+}
+void bubblesort(int *A,int n){
+    int temp;
+    for(int i = 0;i<n-1;i++){
+        for(int j = 0;j < n-i-1;j++){
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
+}    
+int main(){
+    int A[] ={2,5,9,6,4,};
+    int n = sizeof(A)/sizeof(int);
+    printArray(A,n);
+    bubblesort(A,n);
+    printArray(A,n);
+    return 0;
+}
